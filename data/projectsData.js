05d5ecb35150d8847a790f3c92126f4e04ecde02
gatheluck <gatheluck+tech@gmail.com>
2022-07-31 18:00:38 +0900

    feat: add ascender to projects.

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,16 +2,18 @@ const projectsData = {
   en: [
     {
       title: 'Ascender',
-      description: 'Ascender (Accelerator of Scientific Development and Research) is a GitHub repository template for research projects using Python as a developing language.',
-      imgSrc: '/static/images/google.png',
+      description:
+        'Ascender (Accelerator of Scientific Development and Research) is a GitHub repository template for research projects using Python as a developing language.',
+      imgSrc: '/static/images/cvpaperchallenge-16x9.png',
       href: 'https://github.com/cvpaperchallenge/Ascender',
     },
   ],
-  fr: [
+  ja: [
     {
       title: 'Ascender',
-      description: 'Ascender (Accelerator of Scientific Development and Research) is a GitHub repository template for research projects using Python as a developing language.',
-      imgSrc: '/static/images/google.png',
+      description:
+        'Ascender (Accelerator of Scientific Development and Research) is a GitHub repository template for research projects using Python as a developing language.',
+      imgSrc: '/static/images/cvpaperchallenge-16x9.png',
       href: 'https://github.com/cvpaperchallenge/Ascender',
     },
   ],
