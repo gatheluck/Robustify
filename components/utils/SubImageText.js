ab307fe039067a5413b43a444c3356f9e4c0eceb
gatheluck <gatheluck+tech@gmail.com>
2025-01-24 23:30:23 +0900

    style: apply lint.

--- components/utils/SubImageText.js
+++ components/utils/SubImageText.js
@@ -4,7 +4,7 @@ export const SubImageText = ({ classNameDiv = '', classNameText = '', children }
   return (
     <div
       className={ClassNames(
-        'mx-auto -mt-8 -mb-8 flex w-max !p-0 text-center text-xs',
+        'mx-auto -mb-8 -mt-8 flex w-max !p-0 text-center text-xs',
         classNameDiv.length > 0 && classNameDiv
       )}
     >
